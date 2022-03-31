6ede3f3850957505a58eaf91530ff0b4c561a185
boostaboosh <mail.laurencesnow@gmail.com>
2022-03-31 11:41:06 +0100

    Add code to more-on-fractions.c

--- cscu9v4-systems-miniCcourse/week9-moreIntroToC/practical3/more-on-fractions.c
+++ cscu9v4-systems-miniCcourse/week9-moreIntroToC/practical3/more-on-fractions.c
@@ -1,2 +1,34 @@
-/* practical 3 - more on fractions */
+/* more on adding fractions... exercise 1 from practical 3 - practice with I/O */
 
+#include <stdio.h>
+
+int main(void)
+{
+    int num1, denom1, num2, denom2, result_num, result_denom;
+    char addOrMultiply;
+    
+    printf("Enter first fraction: ");
+    scanf("%d/%d", &num1, &denom1);
+
+    printf("Enter second fraction: ");
+    scanf("%d/%d", &num2, &denom2);
+    
+    printf("Enter the character \"a\" if you want to add the two fractions.\nEnter the character \"m\" if you want to multiply the two fractions.\nEntry:\t");
+    scanf("%c", &addOrMultiply);
+
+    if (addOrMultiply == 's') {
+        //summing the two fractions and outputting the result
+        result_num = num1 * denom2 + num2 *denom1;
+        result_denom = denom1 * denom2;
+        printf("The sum is %d/%d\n",result_num, result_denom);
+    }
+
+    if (addOrMultiply == 'm') {
+        //multiplying the two fractions and outputting the result
+        result_num = num1 * num2;
+        result_denom = denom1 * denom2;
+        printf("The product is %d/%d\n",result_num, result_denom);
+    }
+    
+    return 0;
+}
