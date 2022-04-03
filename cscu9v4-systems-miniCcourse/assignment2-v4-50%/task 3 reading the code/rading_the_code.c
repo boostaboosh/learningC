406cfe134057277a5fab879b888d7f89ff143091
boostaboosh <mail.laurencesnow@gmail.com>
2022-04-03 13:18:37 +0100

    Implement task3 ass2 v4 code

---
+++ cscu9v4-systems-miniCcourse/assignment2-v4-50%/task 3 reading the code/rading_the_code.c
@@ -0,0 +1,11 @@
+/* Task 3: Reading the code [10] */
+
+/*
+ * Describe in detail each of the many operations and definitions that are present in the
+ * following code. 
+ */
+
+int main(void){
+for (int i = 0;  i < (int) (sizeof(a) / sizeof(a[0]));  i++)
+	  a[i] = 0;
+}
